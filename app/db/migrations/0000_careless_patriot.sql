fe03772e2b18d89a23396c4c3c1c79cdb7623b4f
nook_owner <nook.dog871@gmail.com>
2024-03-23 17:04:24 +0000

    first commit

--- app/db/migrations/0000_careless_patriot.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`email` text NOT NULL,
-	`tel` text NOT NULL,
-	`memo` text NOT NULL
-);
--- app/db/migrations/0001_luxuriant_unicorn.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `users` DROP COLUMN `tel`;--> statement-breakpoint
-ALTER TABLE `users` DROP COLUMN `memo`;
